cc0ba91a047449fb47a9f48c8e82af2fa0f29c96
Pwpbt <fellipebmartins@gmail.com>
2026-09-14 16:31:59 +0100

    some bullshut that  doesnt work

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class playerscript : MonoBehaviour
 {
     public float Sens = 0.1f;
     public float Speed = 5f;
+    public int DMG;
+    public RawImage KillEffect;
 
     public Rigidbody RB;
     public Camera PlayerCamera;
@@ -12,6 +16,7 @@ public class playerscript : MonoBehaviour
     private Vector2 Dire;
     private Vector2 Look;
     private bool Fired;
+    private int StageKillEffect; //suboptimal
 
     public GameObject BulletHolePrefab;
 
@@ -23,6 +28,9 @@ public class playerscript : MonoBehaviour
     void Start()
     {
         layerMask = LayerMask.GetMask("Default");
+        Color color = KillEffect.color;
+        color.a = 0f;
+        KillEffect.color = color;
     }
 
     void Update()
@@ -32,7 +40,6 @@ public class playerscript : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Movement relative to where the player is facing
         Vector3 move =
             transform.right * Dire.x +
             transform.forward * Dire.y;
@@ -46,40 +53,62 @@ public class playerscript : MonoBehaviour
         );
 
 
-        // Vertical camera rotation
         cameraPitch -= Look.y * Sens;
         cameraPitch = Mathf.Clamp(cameraPitch, -90f, 90f);
 
         PlayerCamera.transform.localRotation =
             Quaternion.Euler(cameraPitch, 0f, 0f);
 
-        // Horizontal player rotation
         transform.Rotate(Vector3.up * Look.x * Sens);
 
-        if (Fired)
-        {
-             RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+       
 
+        if (Fired) //decal do buraco do tiro
         {
-            Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
-                    GameObject bulletHole = Instantiate(
-            BulletHolePrefab,
-            hit.point + hit.normal * 0.001f,
-            Quaternion.LookRotation(-hit.normal)
-        );
+            RaycastHit hit;
+            if (Physics.Raycast(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
 
-        Destroy(bulletHole, 10f);
+            {
+                Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                GameObject bulletHole = Instantiate(BulletHolePrefab,hit.point + hit.normal * 0.001f,Quaternion.LookRotation(-hit.normal));
+                Destroy(bulletHole, 10f);
+                if (hit.transform.gameObject)
+                {
+                    if(hit.transform.gameObject.GetComponent<EnemyScript>() != null)
+                    {
+                        EnemyScript p = hit.transform.gameObject.GetComponent<EnemyScript>();
+                        p.HP -= DMG;
+                        if(p.HP <= 0)
+                        {
+                            StartCoroutine(KillScreenEffect());
+                            //piscar a tela azul estilo gta
+                            //70% clareza
+                            //90% clareza
+                            //desce ate zero
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            }
+            Fired = false;
         }
-        else
-        {
-            Debug.DrawRay(PlayerCamera.transform.position, PlayerCamera.transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
-        }
-        Fired = false;
-        }
+    }
+
+    IEnumerator KillScreenEffect()
+    {
+        KillEffect.CrossFadeAlpha(0.7f, 0.2f, false);
+        Debug.Log("É suposto mudar algo");
+
+        yield return new WaitForSeconds(0.2f);
+
+        KillEffect.CrossFadeAlpha(0.9f, 0.2f, false);
+
+        yield return new WaitForSeconds(0.2f);
+
+        KillEffect.CrossFadeAlpha(0f, 2f, false);
     }
 
     public void OnAttack(InputValue e)
